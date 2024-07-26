b4659bdd22552f1150540de3bf7f677a0cd6b252
mdhasim2001 <mdhasim.nana.2001@gmail.com>
2024-07-27 00:54:58 +0530

    ok

---
+++ JavaScript/App.js
@@ -0,0 +1,7 @@
+document.getElementById("openBtn").addEventListener("click", () => {
+  document.getElementById("navBar").classList.add("navBar");
+});
+
+document.getElementById("closeBtn").addEventListener("click", () => {
+  document.getElementById("navBar").classList.remove("navBar");
+});
